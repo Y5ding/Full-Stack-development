7a08dd772426136830ad4036475e9b4d086ecc5b
Y5ding <y5ding@ucsd.edu>
2021-06-28 15:34:43 -0700

    creat springboot

---
+++ src/main/java/com/yujia/wiki_spring/WikiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.yujia.wiki_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class   WikiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yujia/wiki_spring/WikiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujia.wiki_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
