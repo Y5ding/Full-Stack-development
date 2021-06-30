320484855e0cff771f24d74f4c558481fc49f3b1
Y5ding <y5ding@ucsd.edu>
2021-06-30 16:14:44 -0700

    2-7 client.assert 2-8 configuration:4 properties files+add custom settings

--- src/main/java/com/yujia/wiki_spring/controller/TestController.java
+++ src/main/java/com/yujia/wiki_spring/controller/TestController.java
@@ -1,16 +1,25 @@
 package com.yujia.wiki_spring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //return string or JSON object
 //@Controller return pages
 public class TestController {
-    @RequestMapping("/hello") //accept all types request
+    @Value("${test.hello:Test}")
+    private String testHello;
+    @GetMapping("/hello") //accept all types request
     // GetMapping("use/1") only accept get request
     //GET, POST, PUT, DELETE restful HTTP request
     // use/1 (get userid=1)
     public String hello(){
-        return "Hello Word";
+        return "Hello World "+testHello;
+    }
+    @PostMapping("/hello/post")
+    //parameter delivery  wrong:Map<String, String> map
+    public String helloPost(String name){
+        return  "Hello Word ! Post,"+name;
     }
 }
