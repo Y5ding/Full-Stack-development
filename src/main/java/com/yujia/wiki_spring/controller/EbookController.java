40b5fcc087d85cb941fbf7d4c52c3d80a344c0e0
Y5ding <y5ding@ucsd.edu>
2021-07-08 21:53:07 -0700

    3-6 add CommonResp which is applicable to all interfaces

--- src/main/java/com/yujia/wiki_spring/controller/EbookController.java
+++ src/main/java/com/yujia/wiki_spring/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.yujia.wiki_spring.controller;
 
 import com.yujia.wiki_spring.domain.Ebook;
+import com.yujia.wiki_spring.resp.CommonResp;
 import com.yujia.wiki_spring.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,16 @@ import java.util.List;
 
 @RestController //return string or JSON object
 //@Controller return pages
-@RequestMapping("/Ebook")
+@RequestMapping("/ebook")
 public class EbookController {
     @Resource
     private EbookService ebookService; // input EbookService
 
-    @RequestMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    @GetMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> respond = new CommonResp<>();
+        List<Ebook> list=ebookService.list();
+        respond.setContent(list);
+        return respond;
     }
 }
---
+++ src/main/java/com/yujia/wiki_spring/resp/EbookQueryResp.java
@@ -0,0 +1,4 @@
+package com.yujia.wiki_spring.resp;
+
+public class EbookQueryResp {
+}
