02728e574a60608b3387a854b90fc9461f2105a4
Y5ding <y5ding@ucsd.edu>
2021-07-08 20:08:07 -0700

    3-5 MyBatis Generator-- generate MyBatis interface

---
+++ src/main/java/com/yujia/wiki_spring/domain/Demo.java
@@ -0,0 +1,35 @@
+package com.yujia.wiki_spring.domain;
+
+public class Demo {
+    private Integer id;
+
+    private Integer bookId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", bookId=").append(bookId);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/yujia/wiki_spring/mapper/DemoMapper.java
@@ -0,0 +1,22 @@
+package com.yujia.wiki_spring.mapper;
+
+import com.yujia.wiki_spring.domain.Demo;
+import com.yujia.wiki_spring.domain.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+}
