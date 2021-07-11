12b69a58203411ebdfeacdd0a3993980ae4bf17c
Y5ding <y5ding@ucsd.edu>
2021-07-11 14:49:35 -0700

    5.8 use Spring Boot Interceptor to print the runtime of Request and check Request--excludePathPatterns skip parts

---
+++ src/main/java/com/yujia/wiki_spring/config/SpringMvcConfig.java
@@ -0,0 +1,22 @@
+package com.yujia.wiki_spring.config;
+
+import com.yujia.wiki_spring.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login");
+        // we don't use validation everywhere. Use  excludePathPatterns to ignore not-checked parts
+}
+}
